6bac26a05f2aef3108200053bcaedc47a18ce413
Lee-Dongwook <dlehddnr0713@gmail.com>
2024-03-12 19:09:59 +0900

    chore : nextjs @monorepo/common dependency 추가

--- apps/next/src/app/page.tsx
+++ apps/next/src/app/page.tsx
@@ -1,6 +1,9 @@
+"use client";
 import Image from "next/image";
+import testVariable from "@monorepo/common";
 
 export default function Home() {
+  console.log(testVariable);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
